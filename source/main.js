bd45172898efdf93f5ced9fc7d233512ab43e56d
Kaitlyn Behrens <kaitlynbehrens@ucrwpa-1-7-10-25-26-38.wnet.ucr.edu>
2019-01-19 17:11:29 -0800

    added image:wq

--- source/main.js
+++ source/main.js
@@ -1,5 +1,12 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+// const image = document.getElementById('source');
+
+let mapImage = new Image();
+mapImage.src = "./LMUMap.png";
+console.log('map image is' + mapImage);
+ctx.drawImage(mapImage, 0, 0, 200, 124);
+//drawImage(mapImage);
 
 let classpath = new Graph();
 classpath.addVertex(new Vertex("building", "seaver", 10, 20));
@@ -7,3 +14,4 @@ classpath.addVertex(new Vertex("building", "lsb", 20, 30));
 for (let i = 0; i < classpath.vertices.length; i++) {
      console.log(classpath.vertices[i].name);
 }
+
